021b048ff7397d99599552b31911419cf0c2204c
Katerina <ealmazurova@internet.ru>
2022-03-20 12:55:16 +0300

    Задача на возведение в степень

---
+++ Stepen/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+int A = Prompt("Введите число A => ");
+int B = Prompt("Введите число B => ");
+int degree = Degree(A, B);
+Console.WriteLine($"Число {A} в степени {B} равно {degree}");
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strInput = Console.ReadLine();
+    int answer = int.Parse(strInput);
+    return answer;
+}
+
+int Degree(int aNumber, int bNumber)
+{
+    int degreeNumber = 1;
+    for (int i = 1; i <= bNumber; i++)
+    {
+        degreeNumber = degreeNumber * aNumber;
+    }
+    return degreeNumber;
+}
