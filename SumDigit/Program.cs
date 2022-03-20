663017d381f2f4bbc779303b9cec1e610dd663c0
Katerina <ealmazurova@internet.ru>
2022-03-20 13:18:27 +0300

    Задача на поиск суммы цифр в числе (с функциями)

--- SumDigit/Program.cs
+++ SumDigit/Program.cs
@@ -1,12 +1,19 @@
 ﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-Console.Write("Введите число => ");
-string strInput = Console.ReadLine();
-int[] arr = new int[strInput.Length];
+
+int[] arr = Prompt("Введите число => ");
+Console.WriteLine($"Сумма цифр в введенном числе равна {Sum()}.");
+
+int[] Prompt(string message)
+{
+    Console.Write(message);
+    string strInput = Console.ReadLine();
+    int[] arr = new int[strInput.Length];
     for (int i = 0; i < strInput.Length; i++)
     {
         arr[i] = Convert.ToInt32(strInput.Substring(i, 1));
     }
-Console.WriteLine($"Сумма цифр в введенном числе равна {Sum()}.");
+    return (int[])arr;
+}
 
 int Sum()
 {
