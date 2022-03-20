60a6fb8f060c764ea749fcc24852c679b673b470
Katerina <ealmazurova@internet.ru>
2022-03-20 12:56:23 +0300

    Задача на массив с 8 рандомными элементами через функцию

---
+++ Array/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов случайными числами и выводит их на экран. 
+// Оформите заполнение массива и вывод в виде функции (пригодится в следующих задачах)
+
+void FindArray(int[] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next();         //совсем-совсем случайными.
+    }
+}
+void PrintArray(int[] collect)
+{
+    Console.Write("[");
+    for (int i = 0; i < collect.Length - 1; i++)
+    {
+        Console.Write(collect[i] + ",");
+
+    }
+    Console.Write(collect[collect.Length - 2] + "]");
+}
+
+int[] arr = new int[8];
+
+FindArray(arr);
+PrintArray(arr);
