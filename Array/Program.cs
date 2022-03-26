ca7a8fb881dd78b28ce69afcffc775c2f667d48a
Katerina <ealmazurova@internet.ru>
2022-03-26 09:41:17 +0300

    с учетом комментариев преподавателя

--- Array/Program.cs
+++ Array/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Напишите программу, которая задаёт массив из 8 элементов случайными числами и выводит их на экран. 
 // Оформите заполнение массива и вывод в виде функции (пригодится в следующих задачах)
 
-void FindArray(int[] arr)
+void FillArray(int[] arr)
 {
     Random rnd = new Random();
     for (int i = 0; i < arr.Length; i++)
@@ -17,10 +17,10 @@ void PrintArray(int[] collect)
         Console.Write(collect[i] + ",");
 
     }
-    Console.Write(collect[collect.Length - 2] + "]");
+    Console.Write(collect[collect.Length - 1] + "]");
 }
 
 int[] arr = new int[8];
 
-FindArray(arr);
+FillArray(arr);
 PrintArray(arr);
---
+++ SumDigit_1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int[] arr = Prompt("Введите число => ");
+Console.WriteLine($"Сумма цифр в введенном числе равна {Sum(arr)}.");
+
+int[] Prompt (string message)
+{
+Console.Write(message);
+string strInput = Console.ReadLine();
+int[] arr = new int[strInput.Length];
+    for (int i = 0; i < strInput.Length; i++)
+    {
+        arr[i] = Convert.ToInt32(strInput[i]);
+    }
+return (int[])arr;
+}
+
+int Sum(int[] arr)
+{
+    int sum = 0;
+    for (int j = 0; j < arr.Length; j++)
+    {
+        sum = sum + arr[j];
+    }
+    return sum;
+}
+
+
+//strInput.Substring(i, 1) можно написать так strInput[i]
+
+//Метод int Sum() должен принимать на вход параметр массив, то есть мы передаем в фугкцию массив: int Sum(int[] arr)
+//ну и не забываем при вызове функции Console.WriteLine($"Сумма цифр в введенном числе равна {Sum(arr)}.");
